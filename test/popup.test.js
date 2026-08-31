4c24511e742834a18f69bb0630bdd9143818b9c3
Andrew Landry <andrewlandry@gmail.com>
2026-08-31 17:39:30 -0400

    chore: add Chrome Web Store assets

--- test/popup.test.js
+++ test/popup.test.js
@@ -38,7 +38,9 @@ test("signed-in status is green before active tracking begins", () => {
 
 test("manifest and popup disclose both supported study services", () => {
   assert.equal(manifest.name, "ConvoLab Study Tracker");
-  assert.equal(manifest.version, "0.2.0");
+  assert.equal(manifest.version, "0.2.1");
+  assert.equal(manifest.icons[128], "icons/icon128.png");
+  assert.equal(manifest.action.default_icon[32], "icons/icon32.png");
   assert.ok(manifest.host_permissions.includes("https://www.satorireader.com/*"));
   assert.ok(manifest.content_scripts[0].matches.includes("https://www.satorireader.com/*"));
   assert.match(popupHTML, /WaniKani review and Satori Reader time/);
